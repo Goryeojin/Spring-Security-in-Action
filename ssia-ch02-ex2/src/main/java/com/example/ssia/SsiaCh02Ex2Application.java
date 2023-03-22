21752df205d223f0889ca8672a05fb5365764076
ryeojin <sooon9254@naver.com>
2023-03-22 15:52:37 +0900

    First project, Spring Security Dependency added / Authentication and UserDetailService, PasswordEncoder

---
+++ ssia-ch02-ex2/src/main/java/com/example/ssia/SsiaCh02Ex2Application.java
@@ -0,0 +1,13 @@
+package com.example.ssia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh02Ex2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh02Ex2Application.class, args);
+    }
+
+}
